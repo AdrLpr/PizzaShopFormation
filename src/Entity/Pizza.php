06d94281b86d3017083a9e900b8a22f5999190d0
AdrLpr <adrien.leprince02@live.fr>
2022-04-04 10:33:46 +0200

    Ajout des req et Entity

---
+++ src/Entity/Pizza.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PizzaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PizzaRepository::class)]
+class Pizza
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'float')]
+    private $price;
+
+    #[ORM\Column(type: 'array')]
+    private $ingredient = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getIngredient(): ?array
+    {
+        return $this->ingredient;
+    }
+
+    public function setIngredient(array $ingredient): self
+    {
+        $this->ingredient = $ingredient;
+
+        return $this;
+    }
+}
