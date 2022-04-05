7bbb07c24c86a40e99413649d22ac8fcb92e0e25
AdrLpr <adrien.leprince02@live.fr>
2022-04-05 17:01:27 +0200

    createUserAdmin

--- src/Controller/Admin/UserAdminController.php
+++ src/Controller/Admin/UserAdminController.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Form\SignInType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserAdminController extends AbstractController
@@ -18,6 +21,35 @@ class UserAdminController extends AbstractController
 
         return $this->render('admin/user/retrieve.html.twig', [
             'users'=> $users
+        ]);
+    }
+
+    #[Route('/admin/user/new', "app_admin_user_create")]
+    public function create(UserRepository $repository, UserPasswordHasherInterface $passwordHasher, Request $request ): Response
+    {
+       $form = $this->createForm(SignInType::class);
+
+       $form->handleRequest($request);
+
+       if($form->isSubmitted() && $form->isValid())
+        {
+            $user = $form->getData();
+            $plainpassword= $user->getPassword();
+            
+            $hassedpassword = $passwordHasher->hashPassword(
+                $user,
+                $plainpassword
+            );
+
+            $user->setPassword($hassedpassword);
+
+            $repository->add($user);
+
+            return $this->redirectToRoute('app_admin_user_retrieve');
+        }
+        $formView = $form->createView();
+        return $this->render('admin/user/create.html.twig', [
+            'formView'=>$formView,
         ]);
     }
 }
